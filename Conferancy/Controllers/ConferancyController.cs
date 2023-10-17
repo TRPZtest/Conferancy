d5458afaa220c09ea0498fd484348227dfee45be
DESKTOP-A8GKV9V\DV <dmytro.vyshnevskyi@evinent.com>
2023-10-17 07:49:10 +0300

    Add project files.

---
+++ Conferancy/Controllers/ConferancyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Conferancy.Controllers
+{
+    public class ConferancyController : Controller
+    {
+        public ConferancyController() { }
+        public async Task<ActionResult> Registration()
+        {
+            return View();
+        }
+    }
+}
---
+++ Conferancy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Conferancy.MvcApplication" Language="C#" %>
